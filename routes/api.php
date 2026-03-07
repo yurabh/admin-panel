5f218814ebabbdc11894c9340c93d9fd7a427b02
yurabh <48449309+yurabh@users.noreply.github.com>
2026-03-07 16:54:53 +0200

    Feature 1 work with settings and redis (#1)
    * Add configuration for the redis in config file
    
    * fix
    
    * Performed logic for Setting add controller ,action,resource,requests ,route,observer for remove data from cache , used Redis for retrieve data, and db for storing data and updating data.

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,17 @@ use App\Http\Controllers\Page\ShowPageController;
 use App\Http\Controllers\Page\StorePageController;
 use App\Http\Controllers\Page\UpdatePageController;
 use App\Http\Controllers\Post\PostController;
+use App\Http\Controllers\Setting\SettingController;
 
 Route::prefix('admin')
     ->group(function () {
         Route::resource('posts', PostController::class);
+
         Route::post('/pages', StorePageController::class);
         Route::get('/pages/{page}', ShowPageController::class);
         Route::put('/pages/{page}', UpdatePageController::class);
         Route::delete('/pages/{page}', DeletePageController::class);
+
+        Route::resource('settings', SettingController::class)
+            ->only(['index', 'store', 'update', 'destroy', 'show']);
     });
