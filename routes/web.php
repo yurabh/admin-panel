64a016dd8e0b52ac10455378966e9a29f13415e4
yurabh <48449309+yurabh@users.noreply.github.com>
2026-03-14 00:27:01 +0200

    Add EventServiceProvider for bind event and listeners (#5)
    Add config to the providers config about event service provider
    Dispatch message in RegistrationAction class
    Add Register Event
    Add Listener RegistrationListener for listen RegistrationEvent
    Add route in web.php file
    Add RegistrationEmailConfirmNotification class for prepare for send mail
    Add RegistrationConfirmMail main logic about mailing confirm link regist in the email sended

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/email/verify/{id}/{hash}',
+    function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect('/');
+    })->middleware(['auth:sanctum', 'signed'])
+    ->name('verification.verify');
