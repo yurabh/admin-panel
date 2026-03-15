e97efcf030ad65e22c3a3105cada45a7f5dceec5
Yuriy <yurabh1990@gmail.com>
2026-03-15 21:28:00 +0200

    Add bat cron file in order to run scheduling. Performed command ChangeUserRole in order to change role for user Performed CleanOldComments filter comments to is published true state Performed PublishScheduledPosts command which is change state of posts. Added to the console scheduling for running commands at time indicated in them.

---
+++ app/Console/Commands/CleanOldComments.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Comment;
+use Illuminate\Console\Command;
+
+class CleanOldComments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clean-old-comments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Comment::where("is_approved", false)
+            ->where("created_at", "<", now()->subMinute())
+            ->delete();
+
+        $this->info("Old Comments have been cleaned");
+    }
+}
