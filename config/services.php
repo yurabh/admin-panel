e10a8564282ddc443fd2ccb9b416f12f3268513a
Yuriy <yurabh1990@gmail.com>
2026-07-16 20:44:56 +0300

    Add composer openAI

--- config/services.php
+++ config/services.php
@@ -18,6 +18,8 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'ai_provider' => env('AI_PROVIDER', 'openai'),
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
