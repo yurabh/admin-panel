ba4520d64e717099e823f5da1e9a6268ecfb1b6b
Yuriy <yurabh1990@gmail.com>
2026-07-21 11:05:12 +0300

    Add Shopify config

---
+++ config/shopify.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'shop_domain' => env('SHOPIFY_SHOP_DOMAIN'),
+    'access_token' => env('SHOPIFY_ACCESS_TOKEN'),
+    'api_version' => env('SHOPIFY_API_VERSION', '2024-10'),
+];
